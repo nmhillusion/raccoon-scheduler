dcf386b723e4025ec2d131eea6a12a257aaef5a7
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-10-29 09:14:05 +0700

    update filtered Æá»word and add testing for its

---
+++ src/test/java/app/netlify/nmhillusion/raccoon_scheduler/service_impl/CrawlNewsServiceImplTest.java
@@ -0,0 +1,32 @@
+package app.netlify.nmhillusion.raccoon_scheduler.service_impl;
+
+import app.netlify.nmhillusion.raccoon_scheduler.entity.NewsEntity;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CrawlNewsServiceImplTest {
+    final String[] filteredWords = "bạn gái|cưới|hôn|con giáp|sắc|nhạy cảm|bạn trai|tổ ấm|yêu|tình|hẹn hò".split("\\|");
+
+    private boolean isValidFilteredNews(NewsEntity newsEntity) {
+        return Stream.of(filteredWords).noneMatch(word ->
+                String.valueOf(newsEntity.getTitle()).toLowerCase().contains(String.valueOf(word).toLowerCase())
+                        || String.valueOf(newsEntity.getDescription()).toLowerCase().contains(String.valueOf(word).toLowerCase())
+        );
+    }
+
+    @Test
+    void testFilterNews() {
+        assertFalse(isValidFilteredNews(new NewsEntity().setTitle("Đôi chị em có nhan sắc tuyệt vời")));
+        assertFalse(isValidFilteredNews(new NewsEntity().setTitle("Bạn gái của anh trai đó")));
+        assertFalse(isValidFilteredNews(new NewsEntity().setTitle("2 diễn viên cũ có tình cảm khăn khít")));
+        assertFalse(isValidFilteredNews(new NewsEntity().setTitle("4 con giáp cực tốt trong năm nay")));
+
+        assertTrue(isValidFilteredNews(new NewsEntity().setTitle("Các nhà đầu tư tham gia đầu thầu")));
+        assertTrue(isValidFilteredNews(new NewsEntity().setTitle("Giá xăng dầu có biến động")));
+        assertTrue(isValidFilteredNews(new NewsEntity().setTitle("Intel ra dòng sản phẩm mới")));
+    }
+}
