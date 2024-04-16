a006e45d85e4b739f1b54192a6b1e7ec057a7b55
nmhillusion <nguyenminhhieu.geek@gmail.com>
2024-04-16 22:08:52 +0700

    get random image from unsplash

---
+++ src/main/java/app/netlify/nmhillusion/raccoon_scheduler/service/image/UnsplashImageService.java
@@ -0,0 +1,11 @@
+package app.netlify.nmhillusion.raccoon_scheduler.service.image;
+
+import app.netlify.nmhillusion.raccoon_scheduler.service.BaseSchedulerService;
+
+/**
+ * created by: nmhillusion
+ * <p>
+ * created date: 2024-04-16
+ */
+public interface UnsplashImageService extends BaseSchedulerService {
+}
